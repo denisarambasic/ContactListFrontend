077c1b2836dfa727fd2db4a4455b0549ceb8c668
denisarambasic <denis.arambasic@gmail.com>
2018-04-05 13:43:44 +0200

    Created our first service and registered it into app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app.routing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ContactListComponent } from './contact-list/contact-list.component';
 import { FavoriteListComponent } from './favorite-list/favorite-list.component';
+
+import { ContactService } from './services/contact.service';
 
 @NgModule({
   declarations: [
@@ -14,9 +17,10 @@ import { FavoriteListComponent } from './favorite-list/favorite-list.component';
   ],
   imports: [
     BrowserModule,
+	HttpClientModule,
 	AppRoutingModule
   ],
-  providers: [],
+  providers: [ ContactService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
