83b6863959032e93af7a9608b3d39ef252783d0d
denisarambasic <denis.arambasic@gmail.com>
2018-04-05 13:54:22 +0200

    created our first method in the service to get all contacts from the backend and use it on the contact-list component

--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { ContactService } from '../services/contact.service';
 
 @Component({
 	selector: 'app-contact-list',
 	templateUrl: 'contact-list.component.html',
 	styleUrls: ['contact-list.component.css']
 })
-export class ContactListComponent {
+export class ContactListComponent implements OnInit, OnDestroy {
 	
+	contactSub: any;
 	
+	constructor(private contactService: ContactService){}
+	
+	ngOnInit() {
+		this.contactSub = this.contactService.getAllContacts().subscribe(data => {
+			console.log(data);
+		});
+	}
+	
+	ngOnDestroy() {
+		this.contactSub.unsubscribe();
+	}
 	
 }
--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -3,6 +3,13 @@ import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class ContactService {
-
+	
+	url = 'http://192.168.33.10/typeqast/api/users/';
+	
+	constructor(private http: HttpClient) {}
+	
+	getAllContacts(){
+		return this.http.get(this.url);
+	}
 	
 }
