1367651e6abc6e3424e2b780266870638c9565c9
denisarambasic <denis.arambasic@gmail.com>
2018-04-05 14:17:44 +0200

    install ng-bootstrap library and include it in app.module.ts file to use bootstraps components through angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app.routing';
 import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { ContactListComponent } from './contact-list/contact-list.component';
@@ -18,7 +19,8 @@ import { ContactService } from './services/contact.service';
   imports: [
     BrowserModule,
 	HttpClientModule,
-	AppRoutingModule
+	AppRoutingModule,
+	NgbModule.forRoot()
   ],
   providers: [ ContactService ],
   bootstrap: [AppComponent]
