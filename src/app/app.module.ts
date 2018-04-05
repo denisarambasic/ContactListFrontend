aa6d95605583334a27cb7c0d4b00b391104435ab
denisarambasic <denis.arambasic@gmail.com>
2018-04-05 13:18:38 +0200

    created FavoriteListComponent and registered it in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ContactListComponent } from './contact-list/contact-list.component';
+import { FavoriteListComponent } from './favorite-list/favorite-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-	ContactListComponent
+	ContactListComponent,
+	FavoriteListComponent
   ],
   imports: [
     BrowserModule
