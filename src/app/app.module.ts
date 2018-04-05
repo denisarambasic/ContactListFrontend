1e1e1836e0bd3a42f67460958d19cd0a5ed8b17f
denisarambasic <denis.arambasic@gmail.com>
2018-04-05 13:12:13 +0200

    created contactListComponent and registered it in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { ContactListComponent } from './contact-list/contact-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+	ContactListComponent
   ],
   imports: [
     BrowserModule
